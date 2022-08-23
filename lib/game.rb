2b7ead4a32836ceb47922382c661b19e125227e7
Jozef Hudec <jojohudec27@gmail.com>
2022-08-23 07:34:51 +0200

    Add basic Game class

--- lib/game.rb
+++ lib/game.rb
@@ -55,3 +55,40 @@ HANGMAN_STAGES = ['''
    / \  |
         |
     =========''']
+
+class Game
+    attr_reader :round, :wrong_guesses, :used_letters
+    
+    def initialize(secret_word)
+        @round = 0
+        @wrong_guesses = 0
+        @secret_word = secret_word.split('')
+        @used_letters = []
+        @hint = Array.new(secret_word.length, '_')
+    end
+
+    def guess_letter(letter)
+        self.round = round + 1
+
+        prev_unknown_letter_count = hint.count('_')
+
+        secret_word.length.times { |i| @hint[i] = letter if secret_word[i] == letter } 
+
+        new_unknown_letter_count = hint.count('_')
+
+        self.wrong_guesses = wrong_guesses + 1 if prev_unknown_letter_count == new_unknown_letter_count
+    end
+
+    def used_letter?(letter)
+        used_letters.include?(letter)
+    end
+
+    def hint
+        hint.join('')
+    end
+
+    private
+
+    attr_reader :secret_word
+    attr_writer :round, :wrong_guesses
+end 
--- lib/main.rb
+++ lib/main.rb
@@ -1,3 +1,5 @@
+require_relative 'game'
+
 available_words = File.readlines('words.txt').filter_map { |word| word.chomp if word.chomp.length >= 5 && word.chomp.length <= 12 }
 
-
+g = Game.new(available_words.sample)
