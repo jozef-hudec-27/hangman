ef84623373e584fc03b77d31de5a12956cf71030
Jozef Hudec <jojohudec27@gmail.com>
2022-08-23 08:07:55 +0200

    Add functionality to play

--- lib/game.rb
+++ lib/game.rb
@@ -56,11 +56,13 @@ HANGMAN_STAGES = ['''
         |
     =========''']
 
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+
 class Game
-    attr_reader :round, :wrong_guesses, :used_letters
+    attr_reader :round, :wrong_guesses, :used_letters, :secret_word
     
     def initialize(secret_word)
-        @round = 0
+        @round = 1
         @wrong_guesses = 0
         @secret_word = secret_word.split('')
         @used_letters = []
@@ -69,6 +71,7 @@ class Game
 
     def guess_letter(letter)
         self.round = round + 1
+        used_letters.push(letter)
 
         prev_unknown_letter_count = hint.count('_')
 
@@ -83,12 +86,17 @@ class Game
         used_letters.include?(letter)
     end
 
+    def guess_valid?(guess)
+        return false if guess.length != 1 || used_letter?(guess)
+
+        ALPHABET.include?(guess)
+    end
+
     def hint
-        hint.join('')
+        @hint.join(' ')
     end
 
     private
 
-    attr_reader :secret_word
     attr_writer :round, :wrong_guesses
 end 
--- lib/main.rb
+++ lib/main.rb
@@ -2,4 +2,45 @@ require_relative 'game'
 
 available_words = File.readlines('words.txt').filter_map { |word| word.chomp if word.chomp.length >= 5 && word.chomp.length <= 12 }
 
-g = Game.new(available_words.sample)
+# MAIN LOOP
+loop do
+    game = Game.new(available_words.sample)
+
+    # GAME LOOP
+    loop do
+        puts "> ROUND #{game.round}"
+        puts HANGMAN_STAGES[game.wrong_guesses]
+        puts "> Letters already used: #{game.used_letters.join(', ')}"
+        puts "> Hint: #{game.hint}"
+
+        puts "Please enter a single letter."
+
+        begin
+            guess = gets.chomp.downcase
+            raise 'Invalid guess' unless game.guess_valid?(guess)  
+        rescue 
+            if game.used_letter?(guess)
+                puts "> You have already used this letter. Please pick another one."
+            else
+                puts "> Invalid guess. Please try again."    
+            end
+            retry        
+        else
+            game.guess_letter(guess)
+
+            if game.wrong_guesses == 7
+                puts HANGMAN_STAGES[-1]
+                puts "> You lost! The secret word was #{game.secret_word.join('')}."
+                break
+            elsif game.hint.count('_') == 0
+                puts "You guessed all the letters! The secret word was #{game.hint.split(' ').join('')}."
+                break
+            end
+        end
+    end
+
+    puts "> Do you want to play again? ('y')"
+    play_again = gets.chomp.downcase
+
+    break unless play_again == 'y'
+end
